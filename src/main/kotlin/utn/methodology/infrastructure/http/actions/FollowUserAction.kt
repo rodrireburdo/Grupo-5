fce1a9a36b0bcb32ce7577b9ae8a162592662a7e
Martin <martincrovella@gmail.com>
2024-11-08 17:02:34 -0300

    try

--- src/main/kotlin/utn/methodology/infrastructure/http/actions/FollowUserAction.kt
+++ src/main/kotlin/utn/methodology/infrastructure/http/actions/FollowUserAction.kt
@@ -4,11 +4,11 @@ import utn.methodology.application.commandhandlers.FollowUserHandler
 import utn.methodology.application.commands.FollowUserCommand
 
 class FollowUserAction(
-    private val followUserHandler: FollowUserHandler // Cambié el nombre para claridad
+    private val followUserHandler: FollowUserHandler
 ) {
     fun execute(command: FollowUserCommand): Map<String, String> {
         try {
-            // Aquí manejamos la lógica de seguir a un usuario
+            // Execute the logic to follow a user
             return mapOf("mensaje" to followUserHandler.followUser(command))
         } catch (e: Exception) {
             return mapOf("error" to "Error al seguir al usuario: ${e.message}")
